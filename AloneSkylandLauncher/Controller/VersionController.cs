2a6cede98442d960c5def8cfb79f5dcfea1eb43f
Nikita Chernichkin <nikita.chernichkin@yandex.ru>
2024-07-01 00:19:37 +0300

    Добавьте файлы проекта.

---
+++ AloneSkylandLauncher/Controller/VersionController.cs
@@ -0,0 +1,149 @@
+﻿using Octokit;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AloneSkylandLauncher.Controller
+{
+    public class VersionController
+    {
+        public const string GitName = "DaddyCalcifer", GitRepos = "AloneSkyland";
+        private GitHubClient _githubClient;
+        private string _appDataPath;
+
+        public VersionController()
+        {
+            _githubClient = new GitHubClient(new ProductHeaderValue("AloneSkylanLauncher"));
+            _appDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".AloneSkyland");
+            Directory.CreateDirectory(_appDataPath);
+        }
+
+        public async Task LoadVersions(ComboBox VersionComboBox)
+        {
+            try
+            {
+                var releases = await _githubClient.Repository.Release.GetAll(GitName, GitRepos);
+                VersionComboBox.ItemsSource = releases.Select(r => r.TagName).ToList();
+                VersionComboBox.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка получения версий с сервера: {ex.Message}");
+            }
+        }
+        public async Task DownloadAndLaunchGame(string version, ProgressBar progressBar, System.Windows.Controls.Label statusLabel)
+        {
+            try
+            {
+                var releases = await _githubClient.Repository.Release.GetAll(GitName, GitRepos);
+                var selectedRelease = releases.FirstOrDefault(r => r.TagName == version);
+
+                if (selectedRelease == null)
+                {
+                    MessageBox.Show("Выбранная версия не найдена");
+                    return;
+                }
+
+                var asset = selectedRelease.Assets.FirstOrDefault(a => a.Name.EndsWith(".zip"));
+                if (asset == null)
+                {
+                    MessageBox.Show("Версия не доступна для скачивания.");
+                    return;
+                }
+
+                string downloadPath = Path.Combine(_appDataPath, $"{version.Replace(".", "")}.zip");
+
+                using (var webClient = new WebClient())
+                {
+                    webClient.DownloadProgressChanged += (s, e) =>
+                    {
+                        progressBar.Value = e.ProgressPercentage;
+                        double downloadedMB = e.BytesReceived / 1024.0 / 1024.0;
+                        double totalMB = e.TotalBytesToReceive / 1024.0 / 1024.0;
+                        double speedKBps = e.BytesReceived / 1024.0 / 1024.0 / (e.ProgressPercentage / 100.0);
+                        statusLabel.Content = $"{downloadedMB:F2} MB / {totalMB:F2} MB ({speedKBps:F2} MB/s)";
+                    };
+
+                    webClient.DownloadFileCompleted += (s, e) =>
+                    {
+                        if (e.Error != null)
+                        {
+                            MessageBox.Show($"Ошибка при загрузке файла: {e.Error.Message}");
+                            return;
+                        }
+
+                        // Распаковка архива после завершения загрузки
+                        string versionPath = Path.Combine(_appDataPath, version.Replace(".", ""));
+                        if (Directory.Exists(versionPath))
+                        {
+                            Directory.Delete(versionPath, true);  // Удаляем предыдущую версию, если она существует
+                        }
+
+                        statusLabel.Content = "Распаковка файлов...";
+                        System.IO.Compression.ZipFile.ExtractToDirectory(downloadPath, versionPath);
+                        statusLabel.Content = "Распаковка завершена.";
+                        File.Delete(downloadPath);
+
+                        string gameExecutable = Path.Combine(versionPath, "game\\Alone Skyland.exe");
+                        if (!File.Exists(gameExecutable))
+                        {
+                            MessageBox.Show("Файлы игры повреждены!",
+                            "Ошибка",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Error);
+                            return;
+                        }
+                        statusLabel.Content = "Загрузка завершена.";
+                        MessageBox.Show("Загрузка завершена.");
+                    };
+
+                    // Асинхронная загрузка файла
+                    await webClient.DownloadFileTaskAsync(new Uri(asset.BrowserDownloadUrl), downloadPath);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при установке игры: {ex.Message}",
+                    "Ошибка",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+        public void LaunchGame(string version, string args="")
+        {
+            string versionPath = Path.Combine(_appDataPath, version.Replace(".", ""));
+            string gameExecutable = Path.Combine(versionPath, "game\\Alone Skyland.exe");
+
+            var startInfo = new ProcessStartInfo(gameExecutable)
+            {
+                UseShellExecute = true,
+                WorkingDirectory = versionPath,
+                Arguments = args,
+                //Verb = "runas"  // Запуск от имени администратора
+            };
+
+            try
+            {
+                System.Diagnostics.Process.Start(startInfo);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при запуске игры: {ex.Message}",
+                    "Ошибка запуска",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+        public bool isVersionInstalled(string version)
+        {
+            string versionPath = Path.Combine(_appDataPath, version.Replace(".", ""));
+            string gameExecutable = Path.Combine(versionPath, "game\\Alone Skyland.exe");
+            return File.Exists(gameExecutable);
+        }
+    }
+}
---
+++ AloneSkylandLauncher/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using AloneSkylandLauncher.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AloneSkylandLauncher
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        VersionController versionController;
+        public MainWindow()
+        {
+            InitializeComponent();
+            versionController = new VersionController();
+            versionController.LoadVersions(versionBox).ConfigureAwait(false);
+            loadLabel.Content = String.Empty;
+        }
+
+        private async void downloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            await downloadGame();
+        }
+        async Task downloadGame(bool reinstall=false)
+        {
+            string selectedVersion = versionBox.SelectedItem as string;
+            if(reinstall)
+            {
+                if (MessageBox.Show("Вы уверены что хотите переустановить игру?", "Установка", MessageBoxButton.YesNo) != MessageBoxResult.Yes)
+                    return;
+            }
+            if (string.IsNullOrEmpty(selectedVersion))
+            {
+                MessageBox.Show("Ошибка: Не выбрана версия",
+                    "Ошибка загрузки",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                return;
+            }
+            await versionController.DownloadAndLaunchGame(selectedVersion, loadBar, loadLabel);
+        }
+        private void versionBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            bool isVersionInstalled = false;
+            string selectedVersion = versionBox.SelectedItem as string;
+            if (!string.IsNullOrEmpty(selectedVersion))
+            {
+                isVersionInstalled = versionController.isVersionInstalled(selectedVersion);
+            }
+            else return;
+            if (isVersionInstalled)
+            {
+                PlayButton.IsEnabled = true;
+                downloadButton.Content = "Переустановить";
+            }
+            else
+            {
+                PlayButton.IsEnabled = false;
+                downloadButton.Content = "Загрузить";
+            }
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedVersion = versionBox.SelectedItem as string;
+            if (!string.IsNullOrEmpty(selectedVersion))
+            {
+                versionController.LaunchGame(selectedVersion);
+            }
+            else MessageBox.Show("Ошибка при запуске игры: Не выбрана версия",
+                    "Ошибка запуска",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+        }
+    }
+}
